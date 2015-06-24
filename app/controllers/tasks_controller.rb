e318f8117d3a3560e20bcd399585e0962f0dc95c
Volodymyr Kulyk <dev.volodymyr@gmail.com>
2015-06-24 15:36:09 +0300

    Implemented task creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,24 @@
 class TasksController < ApplicationController
+  before_filter :authenticate_user!
 
   def new
     @task = Task.new
   end
   
+  def create
+    @task = Task.new(task_params)
+    @task.user_id = current_user.id
+    if @task.save
+      redirect_to root_path, :flash => { :success => "Task created successfully" }
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title, :content)
+    end
+
 end
